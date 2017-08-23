7ab0bdd1976ac4dbfdab32846670da5d875e175e
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-23 23:18:01 +0700

    Test for GET /todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos',(req,res) => {
     });
 });
 
+app.get('/todos',(req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(port,()=>{
     console.log('Server is up op port '+port);
 });
