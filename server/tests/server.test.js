128917c1f86858677a71e4daba3fcc8e7b4c749e
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-31 21:41:50 +0700

    Test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,14 +98,24 @@ describe('GET /todos:id',() => {
 
 describe('DELETE /todos:id',() => {
     it('should remove todo By ID',(done) => {
+        var hexId = todos[1]._id.toHexString();
         request(app)
-            .delete(`/todos/${todos[0]._id.toHexString()}`)
+            .delete(`/todos/${hexId}`)
             .expect(200)
             .expect((res) => {
-                expect(res.body.todo.text).toBe(todos[0].text);
+                expect(res.body.todo.text).toBe(todos[1].text);
             })
-            .end(done);
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todos) => {
+                    expect(todos).toNotExist;
+                    done();
+                }).catch((e) => done(e));
+            });
     });
+
 
     it('should return 404 if todo not found',(done) => {
         request(app)
