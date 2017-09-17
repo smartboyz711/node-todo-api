858d7b2863a4c0082cbf9f7a372be2ccbae17f88
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-09-17 19:20:11 +0700

    Add users/login x-auth token

--- server/server.js
+++ server/server.js
@@ -111,7 +111,9 @@ app.post('/users/login',(req,res) => {
         return res.send({error : 'Invaild Data.'});
     }
     User.findByCredentials(body.email, body.password).then((user) => {
-        res.send(user);
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
     }).catch((e) => {
         res.status(400).send({error : e});
     });
